9742fc660ae5d2e027de45c946bec55b205e16cd
thegetoutgirl <amyahaha@gmail.com>
2020-05-20 19:12:57 -0400

    added navbar

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// img: https://cdn.agriland.ie/uploads/2017/08/Sheep-dog-730x375.jpg
+
+export default function Header() {
+return (
+    <div>
+    <h1>hi this is the header</h1>
+</div>
+);
+}
